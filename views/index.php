6dfe48457eabbfaea4f4c2327c7bc59281ab25ee
Sig2510 <34800038+Sig2510@users.noreply.github.com>
2018-02-03 00:38:31 +0200

    02/03/2018 food delivery

--- views/index.php
+++ views/index.php
@@ -3,9 +3,9 @@
 <section class="jumbotron" text-center style="background-image: url(http://res.cloudinary.com/dvs1bvjhz/image/upload/c_crop,h_620,q_100,w_1920,y_250/v1517522904/home_image_qb9k0v.jpg); background-size: 100%;">
   <div class="container" style="margin: 0px 15px; width: 450px;">
     <h1 class="jumbotron-heading" style="color: white">Album example</h1>
-    <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+    <p><b><font color="white">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p></b></font>
     <p>
-      <a href="#" class="btn btn-primary">Main call to action</a>
+      <a href="#" class="btn btn-success">Main call to action</a>
       <a href="#" class="btn btn-secondary">Secondary action</a>
     </p>
   </div>
--- views/partials/header.php
+++ views/partials/header.php
@@ -32,9 +32,8 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>Album</strong>
+          <a href="/index.php?r=/" class="navbar-brand d-flex align-items-center">
+          <strong>Food Delivery</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -59,13 +58,5 @@
             </ul>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="navbar navbar-inverse bg-inverse">
-      <div class="container d-flex justify-content-between">
-        <a href="/index.php?r=/" class="navbar-brand">Food Delivery</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       </div>
     </div>
